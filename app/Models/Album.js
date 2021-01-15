981b959ce48acf20d122fa1d34aba5c35afb6212
Steferson Augusto <steferson_a@hotmail.com>
2021-01-15 14:50:11 -0400

    feat: adiciona cascading delete Remove do min.io e apaga imagens do álbum a ser deletado

--- app/Models/Album.js
+++ app/Models/Album.js
@@ -1,8 +1,25 @@
 'use strict'
 
 const Model = use('Model')
+const Drive = use('Drive')
 
 class Album extends Model {
+  static boot () {
+    super.boot()
+
+    this.addHook('beforeDelete', async (albumInstance) => {
+      try {
+        const images = await albumInstance.images().fetch()
+        for (const image of images.rows) {
+          await image.delete()
+          await Drive.disk('minio').delete(image.name)
+        }
+      } catch (error) {
+        console.log(error)
+      }
+    })
+  }
+
   static get hidden () {
     return ['created_at', 'updated_at']
   }
