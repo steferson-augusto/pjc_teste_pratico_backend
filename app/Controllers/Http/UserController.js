c00ee3ec84dbdbe39a63aef581b4f09332d9436c
Steferson Augusto <steferson_a@hotmail.com>
2021-01-14 22:20:36 -0400

    feat: adiciona prevenção a erros

--- app/Controllers/Http/UserController.js
+++ app/Controllers/Http/UserController.js
@@ -1,21 +1,30 @@
 'use strict'
 
 const User = use('App/Models/User')
+const { responseError } = use('./Helpers/MessageError')
 
 class UserController {
-  async login ({ request, auth }) {
-    const { email, password, refreshToken } = request.all()
+  async login ({ request, auth, response }) {
+    try {
+      const { email, password, refreshToken } = request.all()
 
-    const token = refreshToken
-      ? await auth.generateForRefreshToken(refreshToken, true)
-      : await auth.withRefreshToken().attempt(email, password)
-    return token
+      const token = refreshToken
+        ? await auth.generateForRefreshToken(refreshToken, true)
+        : await auth.withRefreshToken().attempt(email, password)
+      return token
+    } catch {
+      return response.status(500).send(responseError())
+    }
   }
 
   async store({ request, response }) {
-    const data = request.only(['email', 'password', 'username'])
-    const user = await User.create(data)
-    return response.status(201).send(user)
+    try {
+      const data = request.only(['email', 'password', 'username'])
+      const user = await User.create(data)
+      return response.status(201).send(user)
+    } catch {
+      return response.status(500).send(responseError())
+    }
   }
 }
 
