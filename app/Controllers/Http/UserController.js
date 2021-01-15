8bacdd7cc485fa1873c11d238b0c2c6e2fb9d115
Steferson Augusto <steferson_a@hotmail.com>
2021-01-14 22:10:57 -0400

    feat: cria métodos para autenticação e criação de usuários

--- app/Controllers/Http/UserController.js
+++ app/Controllers/Http/UserController.js
@@ -1,7 +1,22 @@
 'use strict'
 
-class UserController {
+const User = use('App/Models/User')
 
+class UserController {
+  async login ({ request, auth }) {
+    const { email, password, refreshToken } = request.all()
+
+    const token = refreshToken
+      ? await auth.generateForRefreshToken(refreshToken, true)
+      : await auth.withRefreshToken().attempt(email, password)
+    return token
+  }
+
+  async store({ request, response }) {
+    const data = request.only(['email', 'password', 'username'])
+    const user = await User.create(data)
+    return response.status(201).send(user)
+  }
 }
 
 module.exports = UserController
