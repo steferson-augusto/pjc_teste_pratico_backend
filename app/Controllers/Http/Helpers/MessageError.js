81c8a4aee2ea492307f06fdc9d31b9d24ba3d67c
Steferson Augusto <steferson_a@hotmail.com>
2021-01-14 22:18:21 -0400

    feat: cria helper para padronização de mensagens de erros

---
+++ app/Controllers/Http/Helpers/MessageError.js
@@ -0,0 +1,30 @@
+const MessageError = {
+    requestFail: [{
+        field: 'general',
+        message: 'Falha na requisição, tente novamente ou contate o administrador',
+    }],
+    loginFail: [{
+        field: 'general',
+        message: 'Falha no login, verifique suas credenciais',
+    }],
+    notFound: [{
+        field: 'general',
+        message: 'O item solicitado não foi encontrado',
+    }],
+    userInvalid: [{
+        field: 'general',
+        message: 'Usuário inválido',
+    }]
+}
+
+const e = 'Falha na requisição, tente novamente ou contate o administrador'
+const responseError = (kind = 'requestFail', errors = [], message = e, field = 'general' ) => {
+    if (kind && MessageError[kind]) {
+        return MessageError[kind]
+    } else {
+      const data = errors.length > 0 ? errors : [{ message, field }]
+      return data
+    }
+}
+
+module.exports = { MessageError, responseError }
