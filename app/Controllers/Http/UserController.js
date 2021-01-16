e35b0ec07a815a558dc56ed7661631d71427ed21
Steferson Augusto <steferson_a@hotmail.com>
2021-01-15 22:26:34 -0400

    feat: cria métodos restantes para rota de users

--- app/Controllers/Http/UserController.js
+++ app/Controllers/Http/UserController.js
@@ -5,7 +5,7 @@ const { validateAll } = use('Validator')
 const User = use('App/Models/User')
 const { responseError } = use('./Helpers/MessageError')
 
-const select = ['users.id', 'users.email', 'users.username']
+const select = ['users.id', 'users.email', 'users.name']
 const rules = {
   email: 'required|email|max:120|min:6'
 }
@@ -42,7 +42,7 @@ class UserController {
       const { direction, columnName, page, perPage, query } = data
       const rulesIndex = {
         direction: 'required|in:asc,desc',
-        columnName: 'required|in:id,email,username',
+        columnName: 'required|in:id,email,name',
         page: 'required|integer|above:-1',
         perPage: 'required|integer|above:2',
       }
@@ -66,11 +66,68 @@ class UserController {
     }
   }
 
-  async store({ request, response }) {
+  async store ({ request, response }) {
     try {
-      const data = request.only(['email', 'password', 'username'])
+      const data = request.only(['email', 'password', 'name'])
+
+      const validation = await validateAll(data, rules, messages)
+      if (validation.fails()) return response.status(400).send(validation.messages())
+
       const user = await User.create(data)
-      return response.status(201).send(user)
+
+      return response.status(201).send({ user, message: 'Usuário criado com sucesso' })
+    } catch {
+      return response.status(500).send(responseError())
+    }
+  }
+
+  async show ({ params, response }) {
+    try {
+      const rulesShow = { id: "required|exists:users,id" }
+      const { id } = params
+
+      const validation = await validateAll({ id }, rulesShow, messages)
+      if (validation.fails()) return response.status(404).send(validation.messages())
+
+      const user = await User.find(params.id)
+
+      return user
+    } catch {
+      return response.status(500).send(responseError())
+    }
+  }
+
+  async update ({ params, request, response }) {
+    try {
+      const rulesUpdate = { ...rules, id: "required|exists:users,id" }
+      const data = request.only(['name'])
+      const { id } = params
+
+      const validation = await validateAll({ ...data, id }, rulesUpdate, messages)
+      if (validation.fails()) return response.status(400).send(validation.messages())
+
+      const user = await User.find(id)
+      user.merge(data)
+      await user.save()
+
+      return response.status(200).send({ user, message: 'Usuário atualizado com sucesso' })
+    } catch {
+      return response.status(500).send(responseError())
+    }
+  }
+
+  async destroy ({ params, response }) {
+    try {
+      const rulesDestroy = { id: "required|exists:users,id" }
+      const { id } = params
+
+      const validation = await validateAll({ id }, rulesDestroy, messages)
+      if (validation.fails()) return response.status(404).send(validation.messages())
+
+      const user = await User.find(params.id)
+      await user.delete()
+
+      return response.status(200).send({ message: 'Usuário apagado com sucesso' })
     } catch {
       return response.status(500).send(responseError())
     }
